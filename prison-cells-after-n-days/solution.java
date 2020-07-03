d42db19b2d9d24847aca09336ea37c9c40701438
Saumya Agarwal <saumya.231997@gmail.com>
2020-07-04 00:08:08 +0530

    It conains solution to leet code day 3 challenge

---
+++ prison-cells-after-n-days/solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        int first[] = new int[8];
+        first[0] = 0;
+        first[7] = 0;
+        
+            for(int j=1;j<7;j++)
+            {
+                if(cells[j-1] == cells[j+1])
+                    first[j] = 1;
+                else
+                    first[j] = 0; 
+            }
+        int count=1;
+        
+        N-=1;
+        
+        for(int i=0; i<8; i++)
+            cells[i] = first[i];
+        
+        while(N-- > 0){
+            int[] temp = new int[8];
+            
+            for(int j=1;j<7;j++)
+            {
+                if(cells[j-1] == cells[j+1])
+                    temp[j] = 1;
+                else
+                    temp[j] = 0; 
+            }
+            
+            if(Arrays.equals(temp,first))
+                N=N%count;
+            for(int j=0;j<8;j++)
+            {
+                cells[j] = temp[j];
+            }
+            count++;
+        }
+        
+        return cells;
+    }
+}
