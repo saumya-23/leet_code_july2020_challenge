c2a024fc75576ad3aaa395cf23410fda414d4301
Saumya Agarwal <saumya.231997@gmail.com>
2020-07-05 15:04:06 +0530

    .

--- Hamming-Distance/solution2.java
+++ Hamming-Distance/solution2.java
@@ -4,17 +4,9 @@ class Solution {
         int result = 0;
         int z = x^y;
         String binaryNum = "";
-        while (z > 0)  
-        { 
-            // storing remainder in binary array 
-            String num = Integer.toString(z % 2);
-            binaryNum = binaryNum + num;
-            z = z / 2; 
-        }
-        for(int i=0;i<binaryNum.length();i++)
-        {
-            if(binaryNum.charAt(i) == '1')
-                result++;
+        while(z>0){
+            ++result;
+            z &= z-1;
         }
         
         return result;
